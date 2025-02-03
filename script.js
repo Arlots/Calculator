f7ed689a7b5f461e5cd3f37c74c9023faa4ef019
Arlots <ethan.jennings092@gmail.com>
2025-02-03 13:40:21 +0900

    created evaulation function based on user input

--- script.js
+++ script.js
@@ -23,6 +23,24 @@ function division(term1, operand, term2) {
     }
 };
 
-let userNumberOne = null;
-let userOperator = null;
-let useNumberTwo = null;
+let userNumberOne = Number(prompt("What is your first number?"));
+let userOperator = prompt("What operator would you like to use?");
+let userNumberTwo = Number(prompt("What is your second number?"));
+
+function evaluate(userNumberOne, userOperator, userNumberTwo) {
+    let result;
+
+    if (userOperator == '+') {
+        result = addition(userNumberOne, userOperator, userNumberTwo);
+    } else if (userOperator == '-') {
+        result = subtraction(userNumberOne, userOperator, userNumberTwo);
+    } else if (userOperator == '*' || userOperator== 'x') {
+        result = multiplication(userNumberOne, userOperator, userNumberTwo);
+    } else if (userOperator == '/') {
+        result = division(userNumberOne, userOperator, userNumberTwo);
+    }
+
+    console.log(result);
+}
+
+evaluate(userNumberOne, userOperator, userNumberTwo);
